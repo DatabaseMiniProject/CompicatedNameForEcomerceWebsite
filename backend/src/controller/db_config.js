6ab012f23b803f38c946c3070553792013fcc7f1
Prajwal-H-Rao <prajwalhr130@gmail.com>
2024-01-21 08:01:59 +0530

    changed normal connection to pool connection

--- backend/src/controller/db_config.js
+++ backend/src/controller/db_config.js
@@ -3,14 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new pg.Client({
+const db_pool = new pg.Pool({
   user: process.env.USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  max:20,
+  idleTimeoutMillis:3000
 });
+
+
 
 const port=process.env.PORT;
 
-export {db,port};
+export {db_pool,port};
--- backend/src/controller/db_query.js
+++ backend/src/controller/db_query.js
@@ -1,27 +1,31 @@
-import { db } from "../controller/db_config.js";
-import {v4} from 'uuid';
+import { db_pool } from "../controller/db_config.js";
+import { v4 } from "uuid";
 
-db.connect();
 const checkDatabase = async (uName, mail) => {
   try {
+    const db = await db_pool.connect();
     const qry =
       "select exists(select 1 from user_table where name=$1 and email=$2)";
     const res = await db.query(qry, [uName, mail]);
+    db.release();
     return res.rows[0].exists;
+  } catch (err) { 
+    console.log(err);
+  }
+};
+
+const insertIntoDatabase = async (uName, mail, pHash) => {
+  try {
+    const db = await db_pool.connect();
+    const qry = "insert into user_table values($1,$2,$3,$4)";
+    const uuidString = v4();
+    const integerId = parseInt(uuidString.slice(0, 4), 16);
+    const res = await db.query(qry, [integerId, uName, mail, pHash]);
+    db.release();
+    return res;
   } catch (err) {
     console.log(err);
   }
 };
 
-const insertIntoDatabase=async(uName,mail,pHash)=>{
-    try{
-        const qry="insert into user_table values($1,$2,$3,$4)";
-        const uuidString = v4();
-        const integerId = parseInt(uuidString.slice(0,4),16);
-        const res=await db.query(qry,[integerId,uName,mail,pHash]);
-        return res;
-    }
-    catch(err){console.log(err)}
-}
-
-export { checkDatabase,insertIntoDatabase };
+export { checkDatabase, insertIntoDatabase };
