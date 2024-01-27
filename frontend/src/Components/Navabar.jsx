3e53ea54d7795da4575783778ed308572d1deb46
dexterzPK <134509031+dexterzPK@users.noreply.github.com>
2024-01-27 14:54:48 +0530

    folder

--- frontend/src/Components/Navabar.jsx
+++ frontend/src/Components/Navabar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import {Link} from "react-router-dom"
 
 function Navabar() {
-  return (
-    <div>Navabar</div>
-  )
+  return <nav>
+    <Link 
+  </nav>
 }
 
-export default Navabar
+export default Navabar;
--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import home from "./home";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Routes>
+      <Route path="/" element={home} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
