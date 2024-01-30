f7d4ddf3a3f6a6163c27c69b10df76e0fb42dac9
Prajwal-H-Rao <prajwalhr130@gmail.com>
2024-01-30 18:17:46 +0530

    Created Cart file and removed manifest.json

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import home from "./home";
 import Men from "./Men";
+import Cart from "./Cart";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={home} />
       <Route path="/men" Component={Men}/>
+      <Route path="/cart" Component={Cart}/>
     </Routes>
   );
 }
---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <div className='cart_page'>
+        <div className='cart_container'>
+            <h1>Cart</h1>
+            <div className='item_container'>
+                Products
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cart
