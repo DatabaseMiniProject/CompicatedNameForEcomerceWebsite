2a8884d06d0937718f56982031cac5c4ef597a7b
Prajwal-H-Rao <prajwalhr130@gmail.com>
2024-02-17 17:30:43 +0530

    saturday is part of weekend!!!

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -3,7 +3,23 @@ import '../assets/styles/Cart.css'
 function Cart() {
   return (
     <div className='cart_page'>
-    
+    <div className='cart_sub-container'>
+    <div className='left-container'>
+    <div className='spacer'></div>
+      <h1 className='cartHeader'>My Cart</h1>
+      <div className='cards'>Item1</div>
+      <div className='cards'>Item1</div>
+      <div className='cards'>Item1</div>
+      <div className='cards'>Item1</div>
+      <div className='cards'>Item1</div>
+      <div className='cards'>Item1</div>
+      <div className='cards'>Item1</div><div className='cards'>Item1</div>
+      <div className='cards'>Item1</div><div className='cards'>Item1</div><div className='cards'>Item1</div><div className='cards'>Item1</div>
+    </div>
+    <div className='right-container'>
+    <div className='spacer'></div>
+    <div className='right-sub-container'>Right</div></div>
+    </div>
     </div>
   )
 }
